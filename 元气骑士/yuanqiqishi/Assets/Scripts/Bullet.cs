28b75f4a000c3b9c78c0c39ca1097ab43cc2c9b6
yang2797817213@qq.com <forestlyn>
2021-03-31 23:27:58 +0800

    2021.3.31change

--- 元气骑士/yuanqiqishi/Assets/Scripts/Bullet.cs
+++ 元气骑士/yuanqiqishi/Assets/Scripts/Bullet.cs
@@ -1,30 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Bullet : MonoBehaviour
 {
     //属性
     public float moveSpeed = 4;
     public bool isPlayerBullet;
-    // Start is called before the first frame update
+    public Vector3 a;
+    
     //引用
     public GameObject BrokenBullet;
-    public GameObject Player;
+    public Transform Player;
+    
 
     void Start()
     {
-        Destroy(gameObject, 2);
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        a = Player.position - transform.position;
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 a;
-        a = Player.transform.position - transform.position;
+        
         if (!isPlayerBullet)
         {
-            transform.Translate(a * 1 * Time.fixedDeltaTime, Space.World);
+            transform.Translate(a * 4 * Time.fixedDeltaTime, Space.World);
         }
         
     }
--- 元气骑士/yuanqiqishi/Assets/Scripts/Enemy.cs
+++ 元气骑士/yuanqiqishi/Assets/Scripts/Enemy.cs
@@ -12,25 +12,18 @@ public class Enemy : MonoBehaviour
 
     public float distance;
     //引用
-    public GameObject player;
+    public Transform player;
     public GameObject EnemyBullet;
-
+    
     private static Enemy instance;
     public static Enemy Instance { get => instance; set => instance = value; }
 
     // Start is called before the first frame update
     
    
- 
-
-  
-    
-
-   
-
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
 
@@ -43,7 +36,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        distance = Vector3.Distance(transform.position, player.transform.position);
+        distance = Vector3.Distance(transform.position, player.position);
         
         if (distance >= 5) Move();
         FindPlayer();
@@ -63,32 +56,39 @@ public class Enemy : MonoBehaviour
         if (timeVal <= 3)
         {
             transform.Translate(Vector3.left * moveSpeed * Time.fixedDeltaTime, Space.World);
+            transform.eulerAngles = new Vector3(0, 180, 0);
         }
         else if (timeVal > 3)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
             transform.Translate(Vector3.right * moveSpeed * Time.fixedDeltaTime, Space.World);
+        }
+
         if (timeVal >= 6) timeVal = 0;
     }
 
     //发现玩家
     private void FindPlayer()
     {
-        if (distance < 5)
+        if (distance < 8)
         {
-            transform.Translate(player.transform.position * Time.fixedDeltaTime * 1, Space.World);
+            Vector3 a = player.transform.position - transform.position;
+            transform.Translate(a * Time.fixedDeltaTime * 1, Space.World);
         }
-        if (distance < 2)
+        if (distance < 4)
         {
             Attack();
         }
     }
+
+    //攻击
     private void Attack()
     {
-        if (timeVal >= 1)
+        if (timeVal >= 3)
         {
-            Instantiate(EnemyBullet, transform.position, player.transform.rotation);
+            Instantiate(EnemyBullet, transform.position, transform.rotation);
             timeVal = 0;
-        }
-            
+        }            
     }
     
 }
